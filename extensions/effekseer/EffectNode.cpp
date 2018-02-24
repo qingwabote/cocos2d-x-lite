d330cdc40806032941e2947888cf2980a9bc837e
DESKTOP-IFMENPM\liuzilong <qingwabote@126.com>
2018-02-24 10:48:54 +0800

    refactor EffectNode

--- extensions/effekseer/EffectNode.cpp
+++ extensions/effekseer/EffectNode.cpp
@@ -80,7 +80,7 @@ void EffectNode::visit(Renderer *renderer, const Mat4& parentTransform, uint32_t
 
 void EffectNode::update(float delta)
 {
-	_emitter->setPosition(_parent->convertToWorldSpace(this->getPosition()));
+	_emitter->setPosition(this->convertToWorldSpaceAR(Vec2::ZERO));
 	_emitter->setScale(this->getScale());
 	_manager->update();
 }
