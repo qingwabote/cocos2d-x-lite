4422b5de08446716ebfecdfb81ac4019b5904e9a
DESKTOP-IFMENPM\liuzilong <qingwabote@126.com>
2018-03-08 09:45:54 +0800

    copy spine files from cocos2dx

--- cocos/editor-support/spine/spine-cocos2dx.cpp
+++ cocos/editor-support/spine/spine-cocos2dx.cpp
@@ -31,13 +31,6 @@
 #include <spine/spine-cocos2dx.h>
 #include <spine/extension.h>
 
-namespace spine {
-	static CustomTextureLoader _customTextureLoader = nullptr;
-	void spAtlasPage_setCustomTextureLoader (CustomTextureLoader texLoader) {
-		_customTextureLoader = texLoader;
-	}
-}
-
 USING_NS_CC;
 
 GLuint wrap (spAtlasWrap wrap) {
@@ -67,13 +60,7 @@ GLuint filter (spAtlasFilter filter) {
 }
 
 void _spAtlasPage_createTexture (spAtlasPage* self, const char* path) {
-	Texture2D* texture = nullptr;
-	if (spine::_customTextureLoader) {
-		texture = spine::_customTextureLoader(path);
-	}
-	if (!texture) {
-		texture = Director::getInstance()->getTextureCache()->addImage(path);
-	}
+	Texture2D* texture = Director::getInstance()->getTextureCache()->addImage(path);
 	CCASSERT(texture != nullptr, "Invalid image");
 	texture->retain();
 
