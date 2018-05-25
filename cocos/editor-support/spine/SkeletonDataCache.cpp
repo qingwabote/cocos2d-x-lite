b105f037153865ad8edb69525fb847a17f29015e
DESKTOP-IFMENPM\liuzilong <qingwabote@126.com>
2018-05-25 17:59:52 +0800

    spine load async

--- cocos/editor-support/spine/SkeletonDataCache.cpp
+++ cocos/editor-support/spine/SkeletonDataCache.cpp
@@ -3,6 +3,10 @@
 #include "Cocos2dAttachmentLoader.h"
 #include <spine/extension.h>
 #include "SkeletonJson.h"
+#include "base/CCDirector.h"
+#include "base/CCScheduler.h"
+
+USING_NS_CC;
 
 static SkeletonDataCache* _instance = nullptr;
 SkeletonDataCache * SkeletonDataCache::getInstance()
@@ -15,12 +19,16 @@ SkeletonDataCache * SkeletonDataCache::getInstance()
 }
 
 SkeletonDataCache::SkeletonDataCache()
+	: _loadingThread(nullptr)
+	, _asyncRefCount(0)
+	, _needQuit(false)
 {
 }
 
 
 SkeletonDataCache::~SkeletonDataCache()
 {
+	CC_SAFE_DELETE(_loadingThread);
 }
 
 spAtlas * SkeletonDataCache::addAtlas(const std::string& file)
@@ -45,7 +53,6 @@ spSkeletonData * SkeletonDataCache::addSkeletonData(const std::string& skeletonD
 	if (!skeletonData) {
 		auto attachmentLoader = SUPER(Cocos2dAttachmentLoader_create(addAtlas(atlasFile)));
 		spSkeletonJson* json = spSkeletonJson_createWithLoader(attachmentLoader);
-		//spSkeletonJson* json = spSkeletonJson_create(addAtlas(atlasFile));
 		json->scale = 1;
 		skeletonData = spSkeletonJson_readSkeletonDataFile(json, skeletonDataFile.c_str());
 		CCASSERT(skeletonData, json->error ? json->error : "Error reading skeleton data file.");
@@ -55,4 +62,97 @@ spSkeletonData * SkeletonDataCache::addSkeletonData(const std::string& skeletonD
 		_skeletonDataMap.insert({ skeletonDataFile, skeletonData });
 	}
 	return skeletonData;
+}
+
+void SkeletonDataCache::addSkeletonDataAsync(const std::string & skeletonDataFile, const std::string & atlasFile, const std::function<void()>& callback)
+{
+	auto it = _skeletonDataMap.find(skeletonDataFile);
+	if (it != _skeletonDataMap.end()) {
+		callback();
+		return;
+	}
+
+	// load atlas on main thread
+	addAtlas(atlasFile);
+
+	if (!_loadingThread){
+		_needQuit = false;
+		_loadingThread = new (std::nothrow) std::thread(&SkeletonDataCache::loadSkeletonData, this);
+	}
+
+	if (0 == _asyncRefCount){
+		Director::getInstance()->getScheduler()->schedule(CC_SCHEDULE_SELECTOR(SkeletonDataCache::addSkeletonDataAsyncCallBack), this, 0, false);
+	}
+
+	++_asyncRefCount;
+
+	AsyncStruct *data = new (std::nothrow) AsyncStruct(skeletonDataFile, atlasFile, callback);
+
+	std::unique_lock<std::mutex> ul(_requestMutex);
+	_requestQueue.push_back(data);
+	_sleepCondition.notify_one();
+}
+
+void SkeletonDataCache::loadSkeletonData()
+{
+	AsyncStruct *asyncStruct = nullptr;
+	while (!_needQuit) {
+		std::unique_lock<std::mutex> ul(_requestMutex);
+		if (_requestQueue.empty()) {
+			asyncStruct = nullptr;
+		} else {
+			asyncStruct = _requestQueue.front();
+			_requestQueue.pop_front();
+		}
+		if (!asyncStruct) {
+			if (_needQuit) break;
+			_sleepCondition.wait(ul);
+			continue;
+		}
+		ul.unlock();
+
+		spSkeletonData* skeletonData = nullptr;
+		auto attachmentLoader = SUPER(Cocos2dAttachmentLoader_create(_atlasMap.find(asyncStruct->atlasFile)->second));
+		spSkeletonJson* json = spSkeletonJson_createWithLoader(attachmentLoader);
+		json->scale = 1;
+		skeletonData = spSkeletonJson_readSkeletonDataFile(json, asyncStruct->skeletonDataFile.c_str());
+		CCASSERT(skeletonData, json->error ? json->error : "Error reading skeleton data file.");
+		spSkeletonJson_dispose(json);
+		spAttachmentLoader_dispose(attachmentLoader);
+		asyncStruct->skeletonData = skeletonData;
+
+		_responseMutex.lock();
+		_responseQueue.push_back(asyncStruct);
+		_responseMutex.unlock();
+	}
+}
+
+void SkeletonDataCache::addSkeletonDataAsyncCallBack(float dt)
+{
+	AsyncStruct *asyncStruct = nullptr;
+	while (true) {
+		_responseMutex.lock();
+		if (_responseQueue.empty()) {
+			asyncStruct = nullptr;
+		}
+		else {
+			asyncStruct = _responseQueue.front();
+			_responseQueue.pop_front();
+		}
+		_responseMutex.unlock();
+
+		if (!asyncStruct) break;
+
+		//TODO if (_skeletonDataMap.find(asyncStruct->skeletonDataFile) != _skeletonDataMap.end())
+		_skeletonDataMap.insert({ asyncStruct->skeletonDataFile, asyncStruct->skeletonData });
+
+		asyncStruct->callback();
+		delete asyncStruct;
+
+		--_asyncRefCount;
+	}
+
+	if (0 == _asyncRefCount){
+		Director::getInstance()->getScheduler()->unschedule(CC_SCHEDULE_SELECTOR(SkeletonDataCache::addSkeletonDataAsyncCallBack), this);
+	}
 }
--- cocos/editor-support/spine/SkeletonDataCache.h
+++ cocos/editor-support/spine/SkeletonDataCache.h
@@ -1,23 +1,58 @@
 #pragma once
 
-#include "base/ccMacros.h"
 #include <string>
 #include <unordered_map>
+#include <queue>
+#include "base/ccMacros.h"
+#include "base/CCRef.h"
 #include "Atlas.h"
 #include "SkeletonData.h"
 
-class SkeletonDataCache
+USING_NS_CC;
+
+class SkeletonDataCache: public Ref
 {
 public:
 	static SkeletonDataCache * getInstance();
 
 	SkeletonDataCache();
-	~SkeletonDataCache();
+	virtual ~SkeletonDataCache();
 
 	spAtlas* addAtlas(const std::string& file);
 	spSkeletonData* addSkeletonData(const std::string& skeletonDataFile, const std::string& atlasFile);
+	void addSkeletonDataAsync(const std::string& skeletonDataFile, const std::string& atlasFile, const std::function<void()>& callback);
+
 private:
 	std::unordered_map<std::string, spAtlas*> _atlasMap;
 	std::unordered_map<std::string, spSkeletonData*> _skeletonDataMap;
+
+	std::thread* _loadingThread;
+	struct AsyncStruct
+	{
+	public:
+		AsyncStruct(const std::string& skeletonDataFile, const std::string atlasFile, std::function<void()> f)
+			: skeletonDataFile(skeletonDataFile), atlasFile(atlasFile), callback(f), skeletonData(nullptr)
+		{}
+
+		~AsyncStruct()
+		{}
+
+		std::string skeletonDataFile;
+		std::string atlasFile;
+		std::function<void()> callback;
+		spSkeletonData* skeletonData;
+	};
+	std::mutex _requestMutex;
+	std::condition_variable _sleepCondition;
+	std::deque<AsyncStruct*> _requestQueue;
+
+	std::mutex _responseMutex;
+	std::deque<AsyncStruct*> _responseQueue;
+
+	int _asyncRefCount;
+	bool _needQuit;
+
+	void loadSkeletonData();
+	void addSkeletonDataAsyncCallBack(float dt);
 };
 
